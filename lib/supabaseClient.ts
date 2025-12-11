33f728594014708ae128c449b94c10cc92ab9d99
gromvs75 <gromvs75@gmail.com>
2025-12-11 01:42:29 +0100

    Commit changes

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,1 +1,6 @@
-// supabase client placeholder
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);// supabase client placeholder
